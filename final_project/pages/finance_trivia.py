e15c10f9b646b7b3744e7635917998a4d53a553b
Ina Alyani <inaalyani1573@gmail.com>
2025-10-01 23:02:55 +0700

    Update finance_trivia.py

--- final_project/pages/finance_trivia.py
+++ final_project/pages/finance_trivia.py
@@ -350,7 +350,15 @@ class QuestionBank: #"""Manages the question database for different languages an
     def get_shuffled_questions(self, lang, diff):
         """Get shuffled list of questions for specified language and difficulty"""
         questions = self.questions[lang][diff].copy()  # Make a copy to avoid modifying original
-        random.shuffle(questions)  # Shuffle the questions
+        random.shuffle(questions)  # Shuffle the questions 
+    # Randomize answer positions so correct answer isn't always A or B
+        for question in questions:
+            options = question['options']
+            correct_answer = options[question['correct']]
+            option_with_correctness = [(opt, opt = correct_answer) for opt in options]
+            random.shuffle(options_with_correctness)
+            random.shuffle(options_with_correctness)
+            question['correct'] = next(i for i, (_, is_correct) in enumerate(options_with_correctness) if is_correct)
         return questions
     
 class ExplanationGenerator:
